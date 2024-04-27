ee81e3c790fdcb8d25c05dbe4f989909fe7bf31c
Sakshij126 <168269106+Sakshij126@users.noreply.github.com>
2024-04-27 23:58:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <first_number> <second_number>");
+            return;
+        }
 
-        // Prompt the user to enter the first number
-        System.out.print("Enter the first number: ");
-        double firstNumber = scanner.nextDouble();
+        double firstNumber = Double.parseDouble(args[0]);
+        double secondNumber = Double.parseDouble(args[1]);
 
-        // Prompt the user to enter the second number
-        System.out.print("Enter the second number: ");
-        double secondNumber = scanner.nextDouble();
-
-        // Add the two numbers
         double sum = firstNumber + secondNumber;
 
-        // Display the result
         System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
-
-        // Close the scanner
-        scanner.close();
     }
 }
